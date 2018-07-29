8855202e1ddbfded0f1b226efdf10b70c10d7169
Tech-Overlord <am900820@gmail.com>
2018-07-29 23:18:42 +0500

    contains fuction definition of system_details which fetches system/os level information via sys & platform libraries.

---
+++ sysinfo/sys_sw_hw_info.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import platform
+import sys
+
+
+def system_details():
+    os_type = platform.system()
+    print('OS Type: ' + os_type)
+    os_name = platform.release()
+    print('OS Release version: ' + os_name)
+    processor_name = platform.processor()
+    print('Processor Name: ' + processor_name)
+    network_name = platform.node()
+    print('Machine Name on Network is: ' + network_name)
+    machine_type = platform.machine()
+    print('Machine Type is: ' + machine_type)
+    system_platform = sys.platform
+    print('System platform type is: ' + system_platform)
